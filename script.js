e28024b528a1f82fdcebf0d7e5d86648c9604ba6
Jquinr <juancarlosquintero@ucompensar.edu.co>
2023-05-25 18:10:16 -0500

    actualizacion 25/2023

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function agregarItem(){
-    const item = document.querySelector('#nombre');
+   const item = document.querySelector('#nombre');
     var lista = document.querySelector('#lista');
-    var nuevonombre = document.createElement("li");
+    var nuevonombre = document.createElement("ol");
     nuevonombre.appendChild(document.createTextNode(item.value));
     lista.appendChild(nuevonombre);
+    
+    const precio = document.querySelector('#precio');
+     var valor = document.querySelector('#valor');
+     var nuevovalor = document.createElement("ol");
+     nuevovalor.appendChild(document.createTextNode(precio.value));
+    valor.appendChild(nuevovalor);
+    
     document.getElementById("item").value = "";
+    document.getElementById("precio").value = "";  
+   
 }
+
+function total({
+    
+})
